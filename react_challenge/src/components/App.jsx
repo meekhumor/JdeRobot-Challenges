8355280615375127f4d9298d5af5e4651e0bbe28
meekhumor <om17092004@gmail.com>
2025-02-26 14:06:30 +0530

    initialize React project with Redux and Tailwind CSS setup

---
+++ react_challenge/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+
+  return (
+    <div className="flex items-center justify-center min-h-screen text-red-500 text-3xl font-bold">
+      Hello
+    </div>
+  );
+};
+
+export default App; 
